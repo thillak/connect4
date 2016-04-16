c3a6de9f235131b8f5de17b5a151f2875111c284
thillak <thillak.sachi@gmail.com>
2016-04-16 14:11:57 -0500

    Connect4
    ayye lmao

---
+++ four.js
@@ -0,0 +1,260 @@
+	 		var playerTurn = "#ff4444";
+	 	 	var color1 = "#ff4444";
+	 	 	var color2 = "#ffff66";
+	 	 	var color3 ="#404040";
+	 	 	var color4 = "#cccccc";
+	 	 	//Fix button 
+	 	 	 $( "#fix" ).on( "click", function() {
+ 					$("#play1").first().css("background-color",color3);		
+ 					$("#play2").first().css("background-color",color3);
+ 					$("#fix" ).first().css("background-color",color4);
+ 					  playerTurn = "#404040";
+ 					 
+				});
+
+
+	 		//Player one and Player 2 button options for color
+	 		 	 $( "#play1" ).on( "click", function() {
+ 					$("#play1").first().css("background-color",color1);		
+ 					$("#play2").first().css("background-color",color3);
+ 					$("#fix" ).first().css("background-color",color3);
+ 					  playerTurn = "#ff4444";
+ 					 
+				});
+
+
+
+	 	 		  $( "#play2" ).on( "click", function() {
+	 	 		  		$("#play2").first().css("background-color",color2);		
+ 						$("#play1").first().css("background-color",color3);
+ 						$("#fix" ).first().css("background-color",color3);
+ 						playerTurn = "#ffff66";
+				});
+
+	 	 		//Function on click for the circles  	
+	 	 		 $( "#1" ).on( "click", function() {
+ 					 $("#1").first().css("background-color",playerTurn);
+				});
+
+ 
+
+	 	 		  $( "#2" ).on( "click", function() {
+ 					 $("#2").first().css("background-color",playerTurn);
+				});
+
+	 	 		 		$( "#3" ).on( "click", function() {
+ 					 $("#3").first().css("background-color",playerTurn);
+				});
+
+ 
+
+	 	 		  $( "#4" ).on( "click", function() {
+ 					 $("#4").first().css("background-color",playerTurn);
+				});
+				
+
+						 $( "#5" ).on( "click", function() {
+ 					 $("#5").first().css("background-color",playerTurn);
+				});
+
+ 
+
+	 	 		  $( "#6" ).on( "click", function() {
+ 					 $("#6").first().css("background-color",playerTurn);
+				});
+
+					
+						 $( "#7" ).on( "click", function() {
+ 					 $("#7").first().css("background-color",playerTurn);
+				});
+
+ 
+
+	 	 		  $( "#8" ).on( "click", function() {
+ 					 $("#8").first().css("background-color",playerTurn);
+				});
+
+
+						 $( "#9" ).on( "click", function() {
+ 					 $("#9").first().css("background-color",playerTurn);
+				});
+
+ 
+
+	 	 		  $( "#10" ).on( "click", function() {
+ 					 $("#10").first().css("background-color",playerTurn);
+				});
+
+
+						 $( "#11" ).on( "click", function() {
+ 					 $("#11").first().css("background-color",playerTurn);
+				});
+
+ 
+
+	 	 		  $( "#12" ).on( "click", function() {
+ 					 $("#12").first().css("background-color",playerTurn);
+				});
+
+
+						 $( "#13" ).on( "click", function() {
+ 					 $("#13").first().css("background-color",playerTurn);
+				});
+
+ 
+
+	 	 		  $( "#14" ).on( "click", function() {
+ 					 $("#14").first().css("background-color",playerTurn);
+				});
+
+						 $( "#15" ).on( "click", function() {
+ 					 $("#15").first().css("background-color",playerTurn);
+				});
+
+ 
+
+	 	 		  $( "#16" ).on( "click", function() {
+ 					 $("#16").first().css("background-color",playerTurn);
+				});
+
+
+						 $( "#17" ).on( "click", function() {
+ 					 $("#17").first().css("background-color",playerTurn);
+				});
+
+ 
+
+	 	 		  $( "#18" ).on( "click", function() {
+ 					 $("#18").first().css("background-color",playerTurn);
+				});
+
+
+						 $( "#19" ).on( "click", function() {
+ 					 $("#19").first().css("background-color",playerTurn);
+				});
+
+ 
+
+	 	 		  $( "#20" ).on( "click", function() {
+ 					 $("#20").first().css("background-color",playerTurn);
+				});
+
+						 $( "#21" ).on( "click", function() {
+ 					 $("#21").first().css("background-color",playerTurn);
+				});
+
+ 
+
+	 	 		  $( "#22" ).on( "click", function() {
+ 					 $("#22").first().css("background-color",playerTurn);
+				});
+
+
+						 $( "#23" ).on( "click", function() {
+ 					 $("#23").first().css("background-color",playerTurn);
+				});
+
+ 
+
+	 	 		  $( "#24" ).on( "click", function() {
+ 					 $("#24").first().css("background-color",playerTurn);
+				});
+
+
+						 $( "#25" ).on( "click", function() {
+ 					 $("#25").first().css("background-color",playerTurn);
+				});
+
+ 
+
+	 	 		  $( "#26" ).on( "click", function() {
+ 					 $("#26").first().css("background-color",playerTurn);
+				});
+
+
+					 $( "#27" ).on( "click", function() {
+ 					 $("#27").first().css("background-color",playerTurn);
+				});
+
+ 
+
+	 	 		  $( "#28" ).on( "click", function() {
+ 					 $("#28").first().css("background-color",playerTurn);
+				});
+
+						 $( "#29" ).on( "click", function() {
+ 					 $("#29").first().css("background-color",playerTurn);
+				});
+
+ 
+
+	 	 		  $( "#30" ).on( "click", function() {
+ 					 $("#30").first().css("background-color",playerTurn);
+				});
+
+
+					 $( "#31" ).on( "click", function() {
+ 					 $("#31").first().css("background-color",playerTurn);
+				});
+
+ 
+
+	 	 		  $( "#32" ).on( "click", function() {
+ 					 $("#32").first().css("background-color",playerTurn);
+				});
+
+				
+						 $( "#33" ).on( "click", function() {
+ 					 $("#33").first().css("background-color",playerTurn);
+				});
+
+ 
+
+	 	 		  $( "#34" ).on( "click", function() {
+ 					 $("#34").first().css("background-color",playerTurn);
+				});
+
+
+					 $( "#35" ).on( "click", function() {
+ 					 $("#35").first().css("background-color",playerTurn);
+				});
+
+ 
+
+	 	 		  $( "#36" ).on( "click", function() {
+ 					 $("#36").first().css("background-color",playerTurn);
+				});
+
+
+						 $( "#37" ).on( "click", function() {
+ 					 $("#37").first().css("background-color",playerTurn);
+				});
+
+ 
+
+	 	 		  $( "#38" ).on( "click", function() {
+ 					 $("#38").first().css("background-color",playerTurn);
+				});
+
+					
+						 $( "#39" ).on( "click", function() {
+ 					 $("#39").first().css("background-color",playerTurn);
+				});
+
+ 
+
+	 	 		  $( "#40" ).on( "click", function() {
+ 					 $("#40").first().css("background-color",playerTurn);
+				});
+
+
+						 $( "#41" ).on( "click", function() {
+ 					 $("#41").first().css("background-color",playerTurn);
+				});
+
+ 
+
+	 	 		  $( "#42" ).on( "click", function() {
+ 					 $("#42").first().css("background-color",playerTurn);
+				});
+		 	
